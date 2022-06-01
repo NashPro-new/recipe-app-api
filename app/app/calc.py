3e3050506e1464d1d8d49039478e61f0c6d9a3be
mark <avinash.sugian@gmail.com>
2022-06-01 12:07:18 +0530

    Configured POSTGRESQL and PSYCOPSG

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator function for testing test module
+"""
+
+
+def add(x,y):
+    """add x and y and return result"""
+    return x+y
+
+def subtract(x,y):
+    """Subbtract x and y and return result"""
+    return x-y
